cfbdfcca035446c3c7d0da6f7cb0e69d7ccb747a
Akleryon <achille.raffin@ecam-strasbourg.eu>
2023-02-22 17:21:08 +0100

    t

--- Processed.py
+++ Processed.py
@@ -5,17 +5,17 @@ sys.path.append("../STOCK_DRL")
 import warnings
 warnings.filterwarnings('ignore')
 
+import itertools
+import pandas as pd
+
 from  module.yahoodownloader import YahooDownloader
-from module.preprocessor import FeatureEngineer
-from module import config
-from module import config_tickers
+from module.preprocessor import FeatureEngineer, data_split
 
 from module.config import (
     DATA_SAVE_DIR,
     TRAINED_MODEL_DIR,
     TENSORBOARD_LOG_DIR,
     RESULTS_DIR,
-    INDICATORS,
     TRAIN_START_DATE,
     TRAIN_END_DATE,
     TEST_START_DATE,
@@ -24,9 +24,38 @@ from module.config import (
     TRADE_END_DATE,
 )
 
-df = YahooDownloader(start_date = TRAIN_START_DATE,
-                     end_date = TRADE_END_DATE,
-                     ticker_list = config_tickers.DOW_30_TICKER,
-                     interval='1D').fetch_data()
+def get_processed_data(interval, ticker, indicators):
 
-                     
+    fe = FeatureEngineer(
+                        use_technical_indicator=True,
+                        tech_indicator_list = indicators,
+                        use_vix=True,
+                        use_turbulence=True,
+                        user_defined_feature = False)
+
+
+    df = YahooDownloader(start_date = TRAIN_START_DATE,
+                        end_date = TRADE_END_DATE,
+                        ticker_list = ticker,
+                        interval=interval).fetch_data()
+
+    processed = fe.preprocess_data(df)
+    list_ticker = processed["tic"].unique().tolist()
+    list_date = list(pd.date_range(processed['date'].min(),processed['date'].max()).astype(str))
+    combination = list(itertools.product(list_date,list_ticker))
+
+    processed_full = pd.DataFrame(combination,columns=["date","tic"]).merge(processed,on=["date","tic"],how="left")
+    processed_full = processed_full[processed_full['date'].isin(processed['date'])]
+    processed_full = processed_full.sort_values(['date','tic'])
+
+    processed_full = processed_full.fillna(0)
+    mvo_df = processed_full.sort_values(['date','tic'],ignore_index=True)[['date','tic','close']]
+
+    train = data_split(processed_full, TRAIN_START_DATE,TRAIN_END_DATE)
+    trade = data_split(processed_full, TRADE_START_DATE,TRADE_END_DATE)
+
+    stock_dimension = len(train.tic.unique())
+    state_space = 1 + 2*stock_dimension + len(INDICATORS)*stock_dimension
+    print(f"Stock Dimension: {stock_dimension}, State Space: {state_space}")
+    
+    return train, trade, stock_dimension, state_space
